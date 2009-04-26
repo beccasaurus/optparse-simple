4a7cb409db1e6942575fbb7bd86ec96190cf4351
remi <remi@remitaylor.com>
2009-04-26 16:44:37 -0700

    added more specs

--- spec/optparse_simple_spec.rb
+++ spec/optparse_simple_spec.rb
@@ -15,7 +15,7 @@ describe OptParseSimple, 'parsing' do
       @foo = nil
 
       opts = parser.new do |opts|
-        opts.on('-f'){ puts "-f block CALLED!"; @foo = 'w00t' }
+        opts.on('-f'){ @foo = 'w00t' }
       end
 
       @foo.should be_nil
@@ -33,7 +33,7 @@ describe OptParseSimple, 'parsing' do
       @foo = nil
 
       opts = parser.new do |opts|
-        opts.on('-f'){ puts "-f block CALLED!"; @foo = 'w00t' }
+        opts.on('-f'){ @foo = 'w00t' }
       end
 
       @foo.should be_nil
@@ -52,7 +52,7 @@ describe OptParseSimple, 'parsing' do
     @foo = nil
 
     opts = OptParseSimple.new do |opts|
-      opts.on('-f'){ puts "-f block CALLED!"; @foo = 'w00t' }
+      opts.on('-f'){ @foo = 'w00t' }
     end
 
     @foo.should be_nil
@@ -70,7 +70,7 @@ describe OptParseSimple, 'parsing' do
     @foo = nil
 
     opts = OptParseSimple.new do |opts|
-      opts.on('-f'){ puts "-f block CALLED!"; @foo = 'w00t' }
+      opts.on('-f'){ @foo = 'w00t' }
     end
 
     @foo.should be_nil
@@ -81,11 +81,147 @@ describe OptParseSimple, 'parsing' do
     @args.should == ['-a', '-x']
   end
 
-  it '--foo'
+  it 'parse --foo' do
+    OptParseSimple.compatibility_mode = false
 
-  it '-f, --foo'
+    @args = ['-a', '--foo', '-x']
+    @foo = nil
 
-  it '-f X'
+    opts = OptParseSimple.new do |opts|
+      opts.on('--foo'){ @foo = 'w00t' }
+    end
+
+    @foo.should be_nil
+
+    opts.parse @args
+
+    @foo.should  == 'w00t'
+    @args.should == ['-a', '--foo', '-x']
+  end
+
+  it 'parse! --foo' do
+    OptParseSimple.compatibility_mode = false
+
+    @args = ['-a', '--foo', '-x']
+    @foo = nil
+
+    opts = OptParseSimple.new do |opts|
+      opts.on('--foo'){ @foo = 'w00t' }
+    end
+
+    @foo.should be_nil
+
+    opts.parse! @args
+
+    @foo.should  == 'w00t'
+    @args.should == ['-a', '-x']
+  end
+
+  it 'parse -f, --foo' do
+    OptParseSimple.compatibility_mode = false
+
+    opts = OptParseSimple.new do |opts|
+      opts.on('-f', '--foo'){ @foo = 'w00t' }
+    end
+
+    # -f
+
+    @args = ['-a', '-f', '-x']
+    @foo = nil
+
+    @foo.should be_nil
+
+    opts.parse @args
+
+    @foo.should  == 'w00t'
+    @args.should == ['-a', '-f', '-x']
+
+    # --foo
+
+    @args = ['-a', '--foo', '-x']
+    @foo = nil
+
+    @foo.should be_nil
+
+    opts.parse @args
+
+    @foo.should  == 'w00t'
+    @args.should == ['-a', '--foo', '-x']
+  end
+
+  it 'parse! -f, --foo' do
+    OptParseSimple.compatibility_mode = false
+
+    opts = OptParseSimple.new do |opts|
+      opts.on('-f', '--foo'){ @foo = 'w00t' }
+    end
+
+    # -f
+
+    @args = ['-a', '-f', '-x']
+    @foo = nil
+
+    @foo.should be_nil
+
+    opts.parse! @args
+
+    @foo.should  == 'w00t'
+    @args.should == ['-a', '-x']
+
+    # --foo
+
+    @args = ['-a', '--foo', '-x']
+    @foo = nil
+
+    @foo.should be_nil
+
+    opts.parse! @args
+
+    @foo.should  == 'w00t'
+    @args.should == ['-a', '-x']
+  end
+
+  it 'parse -f [X]' do
+    OptParseSimple.compatibility_mode = false
+
+    opts = OptParseSimple.new do |opts|
+      opts.on('-f [x]'){|x| @foo = x }
+    end
+
+    # -f
+
+    @args = ['-a', '-f', '4', '-x']
+    @foo = nil
+
+    @foo.should be_nil
+
+    opts.parse @args
+
+    @foo.should  == '4'
+
+    @args.should == ['-a', '-f', '4', '-x']
+  end
+
+  it 'parse! -f [X]' do
+    OptParseSimple.compatibility_mode = false
+
+    opts = OptParseSimple.new do |opts|
+      opts.on('-f [x]'){|x| @foo = x }
+    end
+
+    # -f
+
+    @args = ['-a', '-f', '4', '-x']
+    @foo = nil
+
+    @foo.should be_nil
+
+    opts.parse! @args
+
+    @foo.should  == '4'
+
+    @args.should == ['-a', '-x']
+  end
 
   # TODO i think OptionParser supports the -f=X / --foo=X syntaxes ... i need to add support!
   it '-f=X'
